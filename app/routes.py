11622c1b8c3c3176569f8c9239633913a9a5d9b8
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2025-04-09 23:21:02 -0500

    Add root route with welcome message and navigation hint

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,18 @@ from app import app
 # I'm importing the 'app' object that I created in __init__.py.
 # This gives me access to use decorators like @app.get() to define routes.
 
+@app.get("/")  
+# I'm creating a route for the base URL ("/") of my web app.
+# When someone visits the root of my site, this function will run and return a simple welcome message.
+
+def home():
+    # This is the view function for the root route.
+    # I'm returning a dictionary with a greeting and a hint about using the /aboutme route.
+    return {
+        "message": "Welcome to my Flask app ✨",
+        "try": "/aboutme to view my profile"
+    }
+
 @app.get("/aboutme")  
 # I'm creating a route that listens for GET requests at the URL '/aboutme'.
 # This means when someone visits /aboutme in the browser, the function below will run.
